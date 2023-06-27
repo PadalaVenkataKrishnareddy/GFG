b5abad153eabb389ebcadd553b7d5db6e7fe2552
Krishna Reddy Padala <84383188+PadalaVenkataKrishnareddy@users.noreply.github.com>
2023-06-27 17:41:01 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Max Min/max-min.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findSum(self,A,N): 
+        #code here
+        return min(A)+max(A)
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+
+t=int(input())
+for _ in range(0,t):
+    n=int(input())
+    a=list(map(int,input().split()))
+    ob = Solution()
+    print(ob.findSum(a,n))
+# } Driver Code Ends
