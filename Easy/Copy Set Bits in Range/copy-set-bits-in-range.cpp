670739060c9438ed7a8325073adb34dcccc9aeb1
Krishna Reddy Padala <84383188+PadalaVenkataKrishnareddy@users.noreply.github.com>
2023-07-02 00:50:32 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Copy Set Bits in Range/copy-set-bits-in-range.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution{
+    public:
+    int setSetBit(int x, int y, int l, int r){
+        int c=1;
+        while(y)
+        {
+            if(y&1)
+            {
+                if(c>=l and c<=r)
+                {
+                    x=x | 1<<(c-1);
+                }
+            }
+            y=y>>1;
+            c++;
+            
+        }
+        return x;
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int x, y, l, r;
+        cin>>x>>y>>l>>r;
+        
+        Solution ob;
+        cout<<ob.setSetBit(x, y, l, r)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
