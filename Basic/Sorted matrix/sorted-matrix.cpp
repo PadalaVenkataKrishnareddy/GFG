33d7f7b35017a5b0ddd09632131357bebe6eecfc
Krishna Reddy Padala <84383188+PadalaVenkataKrishnareddy@users.noreply.github.com>
2023-08-19 23:01:24 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Sorted matrix/sorted-matrix.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    vector<vector<int>> sortedMatrix(int n, vector<vector<int>> mat) {
+        // code here
+        priority_queue<int> p;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                p.push(-1*mat[i][j]);
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                mat[i][j]=-1*p.top();
+                p.pop();
+            }
+        }
+        return mat;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        vector<vector<int>> v(N, vector<int>(N));
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++) cin >> v[i][j];
+        Solution ob;
+        v = ob.sortedMatrix(N, v);
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) cout << v[i][j] << " ";
+            cout << "\n";
+        }
+    }
+}
+// } Driver Code Ends
