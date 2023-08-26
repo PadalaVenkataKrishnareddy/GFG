3aacec4a8a71c2e5c7d3271750c67bdf2bca58b3
Krishna Reddy Padala <84383188+PadalaVenkataKrishnareddy@users.noreply.github.com>
2023-08-27 00:03:01 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Reverse a String/reverse-a-string.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+    public:
+    string reverseWord(string str)
+    {
+        // Your code goes hereint 
+        int n=str.size();
+        string re="";
+        for(int i=n-1;i>=0;i--)
+        {
+            re+=str[i];
+        }
+        return re;
+        
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+	
+	int t;
+	cin>>t;
+	while(t--)
+	{
+    	string s;
+    	cin >> s;
+    	Solution ob;
+    	cout << ob.reverseWord(s) << endl;
+	}
+	return 0;
+	
+}
+
+
+// } Driver Code Ends
